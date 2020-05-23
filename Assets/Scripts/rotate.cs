bc7b4f3e62f083a5af06c8c3dc946b32a156eda3
ARVR <arvrsvec2020@gmail.com>
2020-05-23 12:48:03 +0530

    AR Applicatio

---
+++ Assets/Scripts/rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    float speed = 20.0f;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+}
